991e5dfd33ee300b256a1b23dba00dd1f31ab7a4
AdilBendaoud <adilbendaoud@gmail.com>
2023-01-01 15:10:44 +0000

    updating index

--- src/js/courses.js
+++ src/js/courses.js
@@ -12,44 +12,36 @@ range.setAttribute("max", prix);
 
 function creatDiv(imgUrl,titleText,priceText){
   
-  let container = document.createElement("div");
-  container.classList.add("exemple");
-  let imgDiv = document.createElement("div");
-  imgDiv.classList.add("img");
-  let img = document.createElement("img");
-  img.src = imgUrl;
-  imgDiv.appendChild(img);
-  let detail = document.createElement("div");
-  detail.classList.add("detail");
-  let titleDiv = document.createElement("p");
-  titleDiv.classList.add("title");
-  let title = document.createTextNode(titleText);
-  titleDiv.appendChild(title);
-  let priceDiv = document.createElement("p");
-  priceDiv.classList.add("price");
-  let price = document.createTextNode(`${priceText} $`);
-  priceDiv.appendChild(price);
-  detail.appendChild(titleDiv);
-  detail.appendChild(priceDiv);
-  container.appendChild(imgDiv);
-  container.appendChild(detail);
-  div.appendChild(container);
+  let container = document.createElement("div"); //creation d'element div
+  container.classList.add("exemple");// on ajoute la class exemple a l'element div
+  let imgDiv = document.createElement("div");//creation d'element div qui va contenir l'image
+  imgDiv.classList.add("img");//on ajoute la class img a ce dernier
+  let img = document.createElement("img");// creation d'element img
+  img.src = imgUrl;// ajout de lien d'image 
+  imgDiv.appendChild(img);// ajout de l'image a sont div
+  let detail = document.createElement("div");//creation d'element div qui va contenir le titre et le prix
+  detail.classList.add("detail");// ajour du class detail a ce dernier
+  let titleDiv = document.createElement("p");// creation d'element p
+  titleDiv.classList.add("title");//on ajoute la class title a ce dernier
+  let title = document.createTextNode(titleText);//creation du text node qui comporte le titre du cour
+  titleDiv.appendChild(title);//ajout du text node a l'element div
+  let priceDiv = document.createElement("p");// creation d'element p
+  priceDiv.classList.add("price");//on ajoute la class price a ce dernier
+  let price = document.createTextNode(`${priceText} $`);//creation du text node qui comporte le prix du cour
+  priceDiv.appendChild(price);//ajout du text node a l'element p
+ 
+  detail.append(titleDiv, priceDiv);//ajout de titre et prix a l'element div (detail)
+  
+  container.append(imgDiv, detail);//ajout de l'image et le div (detail) au container
+  div.appendChild(container);//ajout du cour dans la page
 }
-
 for (let i = 0; i < courses.length; i++) {
   creatDiv(courses[i].image,courses[i].title,courses[i].price)
 }
 
-function showAllCourses(){
-  for (var i = 0; i < courses.length; i++) {
-    div.children[i].style.display = "block";
-  }
-}
 
-let text = document.createTextNode("Sorry, No Courses Matcher Your Search");
-let p = document.createElement("p");
-p.classList.add("empty");
-p.appendChild(text);
+
+
 
 function displayedElements() {
   var num = 0;
@@ -64,50 +56,87 @@ function displayedElements() {
 //ul
 
 for (var i = 0; i < 5; i++) {
+  //creation d'un event listener pour chaque element <li>
   li[i].addEventListener("click", (e) => {
+
+    // on verifie si l'element cliquer est (All)
     if (e.target.innerText !== "All") {
+      //afficher tous les cours avant d'effectuer le filtrage
       showAllCourses();
+      //filtrage des cours
       for (var i = 0; i < courses.length; i++) {
+        //recuperation du titre de chaque div qui contient les cours
         let title = div.children[i].children[1].children[0].outerText;
-        if (!(title.includes(e.target.innerText) || title.includes(e.target.innerText.toLowerCase()) || title.includes(e.target.innerText.toUpperCase()))) {
+        // si le titre de l'element ne continet pas le mot voulue on le cache
+        if (!(title.includes(e.target.innerText) || 
+              title.includes(e.target.innerText.toLowerCase()) || 
+              title.includes(e.target.innerText.toUpperCase()))) {
+
           div.children[i].style.display = "none";
         }
       }
     } else {
+      //si l'element cliquer est (All) on affiche tous les cours
       showAllCourses();
     }
   });
 }
 
+function showAllCourses(){
+  for (var i = 0; i < courses.length; i++) {
+    div.children[i].style.display = "block";
+  }
+}
+
 //range
 
 range.addEventListener("change", (e) => {
+  //afficher tous les cours avant d'effectuer le filtrage 
   showAllCourses();
   for (var i = 0; i < courses.length; i++) {
+    //recuperation de prix de chaque div qui contient les cours
     let text = div.children[i].children[1].children[1].outerText;
+    
+    //suppression du signe $ avant le filtrage 
     let priceNumber = text.slice(0,text.length-1)
+
+    //si le prix du cours est supérieur a la valeur du input on le cache
     if (+priceNumber > e.target.value) {
       div.children[i].style.display = "none";
     }
   }
+
+  //mise à jour du span qui affiche la valeur du input
   range.nextElementSibling.innerHTML = `valeur : ${e.target.value}$`;
 });
 
 //searsh
 
 searsh.addEventListener("input", (e) => {
+
+  //creation du paragraphe qui va s'afficher si le cour rechercher n'existe pas
+  let text = document.createTextNode("Sorry, No Courses Matcher Your Search");
+  let p = document.createElement("p");
+  p.classList.add("empty");
+  p.appendChild(text);
+
+  //verification s'il existe un cours 
   if (div.getElementsByClassName("empty").length > 0) {
     div.removeChild(p);
   }
 
+  //afficher tous les cours avant d'effectuer le filtrage
   showAllCourses();
-  
+
+  //filtrage des cours
   for (var i = 0; i < courses.length; i++) {
-    var text = e.target.value;
-    if (!div.children[i].children[1].children[0].outerText.includes(text)) {
+    var inputValue = e.target.value;
+    if (!div.children[i].children[1].children[0].outerText.includes(inputValue)) {
       div.children[i].style.display = "none";
     }
   }
+
+  //affichage du paragraphe s'il n'y a pas de cour
   if (displayedElements() === 0) {
     div.appendChild(p);
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,46 @@
 import courses from "./data.js";
 let container = document.getElementById("container");
 
+function creatElm(imgUrl, titleText, priceText) {
+  let card = document.createElement("div"); //creaction d'un element div
+  card.classList.add("swiper-slide", "card"); //ajout des classes swiper-slide et card a notre element card
 
+  let content = document.createElement("div"); //creation d'element div qui va contenir l'image, le titre et le prix
+  content.classList.add("card-content"); //ajout du class card-content a content
 
-function creatElm(imgUrl,titleText,priceText){
-  let card = document.createElement("div");
-  card.classList.add("swiper-slide");
-  card.classList.add("card");
-  let content = document.createElement("div");
-  content.classList.add("card-content");
-  let imgDiv = document.createElement("div");
-  imgDiv.classList.add("image");
-  let img = document.createElement("img");
-  img.src = imgUrl;
-  imgDiv.appendChild(img);
-  let para = document.createElement("div");
-  para.classList.add("paragraphe");
-  let titleSpan = document.createElement("span");
-  titleSpan.classList.add("title");
-  let title = document.createTextNode(`${titleText}`);
-  titleSpan.appendChild(title);
-  let priceSpan = document.createElement("span");
-  priceSpan.classList.add("price");
-  let price = document.createTextNode(`${priceText} $`);
-  priceSpan.appendChild(price);
-  para.appendChild(titleSpan);
-  para.appendChild(priceSpan)
-  content.appendChild(imgDiv);
-  content.appendChild(para);
-  card.appendChild(content);
-  container.appendChild(card);
+  let imgDiv = document.createElement("div"); //creaction d'element div qui va contenir l'imge
+  imgDiv.classList.add("image"); //ajout du class image a imgDiv
+  let img = document.createElement("img"); //creation d'element img
+  img.src = imgUrl; //on ajoute le lien d'image a l'element img
+  imgDiv.appendChild(img); //on ajoute l'image a sont div
+
+  let para = document.createElement("div"); //creaction d'element div qui va contenir le titre et le prix
+  para.classList.add("paragraphe"); // ajout du class paragraphe a para
+  let titleSpan = document.createElement("span"); //creaction d'element span qui va contenir le titre
+  titleSpan.classList.add("title"); // ajout du class title a titleSpan
+  let title = document.createTextNode(`${titleText}`); //creation d'un text node qui contient le titre
+  titleSpan.appendChild(title); // on ajoute le titre a l'element span
+  let priceSpan = document.createElement("span"); //creaction d'element span qui va contenir le prix
+  priceSpan.classList.add("price"); // ajout du class price a priceSpan
+  let price = document.createTextNode(`${priceText} $`); //creation d'un text node qui contient le prix
+  priceSpan.appendChild(price); // on ajoute le prix a sont l'element span
+
+  para.appendChild(titleSpan); //ajout du span de titre a un div
+  para.appendChild(priceSpan); //ajout du span de prix a un div
+
+  content.appendChild(imgDiv); //ajout du div qui contient l'image a un div (content)
+  content.appendChild(para); //ajout du div qui contient le titre et le prix a un div (content)
+  card.appendChild(content); // ajout du div (content) a l'element div (card)
+  container.appendChild(card); //ajout du div (card) a l'element qui contient tous les cours
 }
 
 let numExist = [];
 
 for (var i = 0; i < 9; i++) {
-
-  let num = Math.ceil(Math.random() * courses.length-1);
-  while(numExist.includes(num)){
-    num = Math.ceil(Math.random() * courses.length-1);
+  let num = Math.ceil(Math.random() * courses.length - 1);
+  while (numExist.includes(num)) {
+    num = Math.ceil(Math.random() * courses.length - 1);
   }
-  console.log(num)
   numExist.push(num);
-  creatElm(courses[num].image,courses[num].title,courses[num].price)
-  
+  creatElm(courses[num].image, courses[num].title, courses[num].price);
 }
